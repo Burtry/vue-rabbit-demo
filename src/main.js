140e4433e3e0a8dc4179ed06c05d2e587cc82707
Burtry <1971797331@qq.com>
2024-04-24 10:14:52 +0800

    一级导航渲染

--- src/main.js
+++ src/main.js
@@ -7,11 +7,8 @@ import './style/common.scss'
 import App from './App.vue'
 import router from './router'
 
-import { getCategoryAPI } from './api/category'
 const app = createApp(App)
-getCategoryAPI().then(res => {
-    console.log(res);
-})
+
 app.use(createPinia())
 app.use(router)
 
