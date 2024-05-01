3d47bf2126493296bc28a309c5df0aebd4c928f6
Burtry <1971797331@qq.com>
2024-05-01 09:57:05 +0800

    一级导航-面包屑导航渲染

--- src/api/category.js
+++ src/api/category.js
@@ -4,3 +4,13 @@ import instance from "@/utils/http"
 export function getCategoryAPI() {
     return instance.get("home/category/head")
 }
+
+//获取二级分类
+export function getSubCategoryAPI(id) {
+    return instance({
+        url: '/category',
+        params: {
+            id
+        }
+    })
+}
