4a4789abd480ffa9100dac1dba66b3c0954baffe
Burtry <1971797331@qq.com>
2024-05-01 16:02:25 +0800

    二级分类的路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '@/views/Home/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Category from '@/views/Category/category.vue'
 import Login from '@/views/Login/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
           path: 'category/:id',
           name: 'category',
           component: Category,
+        },
+        {
+          path: 'category/sub/:id',
+          name: 'subcategory',
+          component: SubCategory,
         }
       ]
     },
