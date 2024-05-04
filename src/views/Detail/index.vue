8f17f17b8167022f9f69c5acd27379be745edd3f
Burtry <1971797331@qq.com>
2024-05-04 10:14:55 +0800

    基础组件封装+基础数据渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,8 @@
 import { getDetailAPI } from '@/api/detail'
 import { ref, onMounted } from 'vue'
 import { useRoute } from "vue-router";
+import DetailHot from './components/DetailHot.vue'
+
 const route = useRoute()
 
 const goods = ref({})
@@ -120,7 +122,8 @@ onMounted(() => {
                         </div>
                         <!-- 24热榜+专题推荐 -->
                         <div class="goods-aside">
-
+                            <DetailHot></DetailHot>
+                            <DetailHot></DetailHot>
                         </div>
                     </div>
                 </div>
