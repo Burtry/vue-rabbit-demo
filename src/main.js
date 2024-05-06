98389ab7fe2b52ecc18d10dad85007b0b7c08b69
Burtry <1971797331@qq.com>
2024-05-06 16:41:00 +0800

    pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ import { createPinia } from 'pinia'
 import './style/common.scss'
 import { lazyPlugin } from '@/directives/lazyPlugin'
 import { componentsPlugins } from "@/components/index";
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentsPlugins)
