6553e1e4edde65da07aef2d56b6987c11fec005b
Burtry <1971797331@qq.com>
2024-05-06 17:37:23 +0800

    token失效拦截

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from "@/stores/user";
-
+import router from '@/router';
 
 
 // 创建axios实例
@@ -14,6 +14,7 @@ const instance = axios.create({
 // axios请求拦截器
 instance.interceptors.request.use(config => {
     const userStore = useUserStore()
+
     const token = userStore.userInfo.token
     if (token) {
         config.headers.Authorization = `Bearer ${token}`
@@ -23,7 +24,13 @@ instance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 instance.interceptors.response.use(res => res.data, e => {
+    const userStore = useUserStore()
     ElMessage({ message: e.response.data.message, type: 'error' })
+    //401处理
+    if (e.response.status === 401) {
+        userStore.removeUserInfo()
+        router.push('/login')
+    }
     return Promise.reject(e)
 })
 
